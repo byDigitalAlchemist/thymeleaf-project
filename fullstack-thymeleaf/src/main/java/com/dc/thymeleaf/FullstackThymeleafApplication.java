d46ecb9f1023a46efa8755c4e0a98c45943fb29d
Anwaar <developer.anwar@gmail.com>
2018-02-08 14:47:23 -0500

    Thymeleaf
    1- Project to learn thymeleaf

---
+++ fullstack-thymeleaf/src/main/java/com/dc/thymeleaf/FullstackThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.dc.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackThymeleafApplication.class, args);
+	}
+}
